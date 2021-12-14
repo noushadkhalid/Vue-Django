46bf67cc70185d11456458f87868820f897785f7
noushadkhalid <noushadkhalid@gmail.com>
2021-12-14 16:28:26 +1100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+          <router-link class="nav-link active" aria-current="page" :to="{ name:'Home'}">Home</router-link> 
+          <router-link class="nav-link" :to="{ name:'About'}">About</router-link>
+          <router-link class="nav-link" :to="{ name:'Posts'}">Posts</router-link>
+       
+        
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Posts.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <h1>Posts</h1>
+        <div class="card-group">
+  <div class="card" v-for="post in APIData" :key="post.id">
+    
+    <div class="card-body">
+      <h5 class="card-title">{{post.title}}</h5>
+      <p class="card-text">{{post.content}}</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  
+</div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+// import {getAPI} from '@/axios-api'
+
+    export default {
+        name:'Posts',
+        data(){
+            return{
+                APIData:[],
+            }
+        },
+         mounted() {
+      axios({
+      method: "GET",
+      url: "http://127.0.0.1:8000/posts/",
+    })
+      .then((response) => {
+       
+        if(response.data.status == true){
+           this.APIData = response.data
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+
+
+
+    // created () {
+    //     getAPI.get('/posts/',)  
+    //       .then(response => {
+    //        console.log('Post APi recieved')
+    //        this.APIData = response.data
+    //       })
+    //       .catch(err =>{ 
+    //           console.log(err)
+    //       })
+      
+
+    // }
+    }
+    
+</script>
+
+<style scoped>
+
+</style>
